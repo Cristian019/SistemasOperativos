d0c9f9a3005101b338f56c96e1d6f93155b90736
cristian Escobar <cristian.escobar@upb.edu.co>
2021-08-29 21:20:08 -0500

    feature3

--- evaluacionUnidad1-2021-20-main/student.c
+++ evaluacionUnidad1-2021-20-main/student.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "student.h"
@@ -22,6 +23,9 @@ void feature1(FILE *fin, FILE *fout){
 
 void feature2(FILE *fin, FILE *fout){
  	char caracteres[100];
+    int longitudCadena = strlen(caracteres);
+    char cadenaInvertida[longitudCadena];
+    int contador = 0;
  	if (fin == NULL)
         printf("\nError de apertura del archivo. \n\n");
  	else
@@ -31,23 +35,51 @@ void feature2(FILE *fin, FILE *fout){
         }
     }
     fclose(fin);
-	int longitud = strlen(caracteres);
-    char invertido[longitud];
-    int contador = 0;
-    for (int i = longitud-1; i >= 0; i--)
+    for (int i = longitudCadena-1; i >= 0; i--)
     {
-        invertido[contador++] = caracteres[i];
+        cadenaInvertida[contador++] = caracteres[i];
     }  
-    fputs(invertido,fout);
+    fputs(cadenaInvertida,fout);
     fclose(fout);
 }
 void feature3(FILE *fin, FILE *fout){
+    char caracteres[100];
+ 	if (fin == NULL)
+        printf("\nError de apertura del archivo. \n\n");
+ 	else
+    {
+        for(int i = 0;(feof(fin) == 0)&& i <3; i++){
+            fgets(caracteres,100,fin);
+        }
+    }
+    int longitudCadena = strlen(caracteres);
+    fclose(fin);
+    int numeros[15];
+    int j;
+    int cont = 0;
+    int sumaNumero = 0;
+    char suma;
+    for (int i = 0; i < longitudCadena-2; i+=2)
+   {
+        sscanf(caracteres+i, "%d", &j); 
+        printf("%d\n",cont,i,j);
+        sumaNumero +=j;
+        if (cont++ == 2)
+        {
+            i++;
+            cont = 0;
+        }
+        if(i==21){
+            i+=2;
+        }
+    }
+    suma = (char) sumaNumero;
+    fprintf(fout,"\nel resultado de la suma es: %d",suma);
+}
+void feature4(FILE *fin, int **parr, int *length, char **op){
 
 }
-/*void feature4(FILE *fin, int **parr, int *length, char **op){
-
-}
-void feature5(FILE *fout, int *parr, int length, char *op){
+/*void feature5(FILE *fout, int *parr, int length, char *op){
 
 }
 void feature6(FILE *fin, struct Obj_t *pobj){
